565d9b2229bb4e06603b642965d247fad65a6de6
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-03-15 11:42:20 -0300

    prepareProd.sh

--- prepareProd.sh
+++ prepareProd.sh
@@ -1,6 +1,21 @@
+echo "dumping routes para json..."
 php bin/console fos:js-routing:dump --format=json --target=assets/static/fos_js_routes.json
+echo "ok!"
+
+echo "limpando cache de prod..."
 php bin/console cache:clear --env=prod
+echo "ok!"
+
+echo "warming up cache..."
 php bin/console cache:warmup
+echo "ok!"
+
+echo "ajustando permissões de var e vendor..."
 chmod -Rf 0777 ./var/
 chmod -Rf 0777 ./vendor/symfony/cache/
+echo "ok!"
+
+echo "yarn encore production..."
 yarn encore production
+echo "ok!"
+
