739606c5a54359af9b70630f5b24f25fa67d870e
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-05-24 17:28:07 -0300

    ...

--- src/Business/Security/SecurityBusiness.php
+++ src/Business/Security/SecurityBusiness.php
@@ -3,11 +3,8 @@
 namespace App\Business\Security;
 
 
-use App\Entity\Config\App;
 use Symfony\Bridge\Doctrine\RegistryInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * Class SecurityBusiness.
@@ -27,24 +24,5 @@ class SecurityBusiness
         $this->authChecker = $authChecker;
     }
 
-    /**
-     * @param Controller $controller
-     * @param $appDesc
-     * @throws \Exception
-     */
-    public function checkAccess($appDesc)
-    {
-        $app = $this->doctrine->getRepository(App::class)->findOneby(['route' => $appDesc]);
-        if (!$app) {
-            throw new \Exception('App não encontrado: [' . $appDesc . ']');
-        }
-        if (!$app->getRoles() or $app->getRoles()->count() < 1) {
-            throw new \Exception('App sem roles definidas: [' . $appDesc . ']');
-        }
-        if (!$this->authChecker->isGranted($app->getRolesArray())) {
-            throw new AccessDeniedException();
-        }
-
-    }
 
 }
