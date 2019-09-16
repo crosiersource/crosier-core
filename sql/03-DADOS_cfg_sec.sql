265604bd3da2da4e62dd44994735cb752332a42c
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-09-16 17:27:35 -0300

    ...

--- sql/03-DADOS_cfg_sec.sql
+++ sql/03-DADOS_cfg_sec.sql
@@ -12,7 +12,7 @@ TRUNCATE TABLE sec_user_role;
 
 INSERT INTO cfg_estabelecimento(id,codigo,descricao,concreto,pai_id,updated,inserted,user_inserted_id,user_updated_id) VALUES(1,1,'ADMIN',true,null,now(),now(),1,1);
 
-INSERT INTO sec_user(id,username,nome,email,password,ativo,group_id,estabelecimento_id,updated,inserted,user_inserted_id,user_updated_id) VALUES(1,'admin','Admin','','$2y$13$NO92WWcpohWwurgTAtG5e.TBRjo2Jp/p3mSfnvNvsFCmtkD4lwqy.',true,1,1,now(),now(),1,1);
+INSERT INTO sec_user(id,username,nome,email,password,ativo,group_id,estabelecimento_id,updated,inserted,user_inserted_id,user_updated_id) VALUES(1,'admin','Admin','','$argon2id$v=19$m=65536,t=4,p=1$sBsDp1zTn/Bfu3uOhRLDwg$TnFcbthOzeMb13njZlGLYjuRCZ16+xO4YHa+wX1yYaU',true,1,1,now(),now(),1,1);
 
 
 
