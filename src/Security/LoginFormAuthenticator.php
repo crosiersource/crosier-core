bdc4c0b5b40d08a0a8932db4401b207e5287d585
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-03-16 16:38:32 -0300

    Alterando sistema de login (adicionando 'login_throttling')

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,7 +2,6 @@
 
 namespace App\Security;
 
-use CrosierSource\CrosierLibBaseBundle\Business\Config\SyslogBusiness;
 use CrosierSource\CrosierLibBaseBundle\Entity\Security\User;
 use CrosierSource\CrosierLibBaseBundle\EntityHandler\Security\UserEntityHandler;
 use CrosierSource\CrosierLibBaseBundle\Repository\Security\UserRepository;
@@ -10,16 +9,23 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
-use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 /**
@@ -29,7 +35,7 @@ use Symfony\Component\Security\Http\Util\TargetPathTrait;
  *
  * @author Carlos Eduardo Pauluk
  */
-class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+class LoginFormAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
 {
     use TargetPathTrait;
 
@@ -44,67 +50,53 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
     private UserEntityHandler $userEntityHandler;
 
     private Security $security;
-    
+
     private LoggerInterface $logger;
 
-    private SyslogBusiness $syslog;
 
-    public function __construct(UserRepository $userRepository,
-                                RouterInterface $router,
-                                CsrfTokenManagerInterface $csrfTokenManager,
-                                UserPasswordEncoderInterface $passwordEncoder,
-                                UserEntityHandler $userEntityHandler,
-                                LoggerInterface $logger,
-                                Security $security,
-                                SyslogBusiness $syslog)
-    {
-        $this->userRepository = $userRepository;
-        $this->router = $router;
-        $this->csrfTokenManager = $csrfTokenManager;
-        $this->passwordEncoder = $passwordEncoder;
-        $this->userEntityHandler = $userEntityHandler;
-        $this->logger = $logger;
-        $this->security = $security;
-        $this->syslog = $syslog;
-    }
-
-    public function supports(Request $request)
+    public function supports(Request $request): ?bool
     {
         return $request->attributes->get('_route') === 'login' && $request->isMethod('POST');
     }
 
-    public function getCredentials(Request $request)
+
+    public function authenticate(Request $request): PassportInterface
     {
-        $credentials = [
-            'username' => $request->request->get('username'),
-            'password' => $request->request->get('password'),
-            'csrf_token' => $request->request->get('_csrf_token'),
-        ];
+        $csrfToken = $request->request->get('_csrf_token');
+        $username = $request->request->get('username');
+        $plaintextPassword = $request->request->get('password');
 
-        $request->getSession()->set(
-            Security::LAST_USERNAME,
-            $credentials['username']
-        );
+        $request->getSession()->set(Security::LAST_USERNAME, $username);
 
-        return $credentials;
-    }
-
-    public function getUser($credentials, UserProviderInterface $userProvider)
-    {
-        $token = new CsrfToken('authenticate', $credentials['csrf_token']);
+        $token = new CsrfToken('authenticate', $csrfToken);
         if (!$this->csrfTokenManager->isTokenValid($token)) {
             throw new InvalidCsrfTokenException();
         }
 
-        return $this->userRepository->findOneBy(['username' => $credentials['username']]);
+        return new Passport(new UserBadge($username), new PasswordCredentials($plaintextPassword));
     }
 
-    public function checkCredentials($credentials, UserInterface $user)
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+        if ($exception instanceof TooManyLoginAttemptsAuthenticationException) {
+            $errMsg = [
+                'messageKey' => 'Login bloqueado (Causa: muitas tentativas de login)'
+            ];
+        } elseif ($exception instanceof BadCredentialsException) {
+            $errMsg = [
+                'messageKey' => 'Usuário ou senha inválidos'
+            ];
+        } else {
+            $errMsg = [
+                'messageKey' => 'Erro ao efetuar login'
+            ];
+        }
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $errMsg);
+        return new RedirectResponse($this->router->generate('login'));
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         try {
             /** @var User $user */
@@ -112,19 +104,15 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
             $this->userEntityHandler->save($user);
             $this->userEntityHandler->renewTokenApi($user);
             $this->userEntityHandler->fixRoles($user);
-            $this->syslog->info('core', self::class, 'onAuthenticationSuccess');
-            
+
             $cache_entmenulocator = new FilesystemAdapter('entmenulocator', 0, $_SERVER['CROSIER_SESSIONS_FOLDER']);
             $cache_entmenulocator->clear();
 
-            
+
             $cacheCrosierCoreAssetExtension = new FilesystemAdapter('CrosierCoreAssetExtension', 0, $_SERVER['CROSIER_SESSIONS_FOLDER']);
             // limpa os cachês definidos em CrosierSource\CrosierLibBaseBundle\Twig\CrosierCoreAssetExtension
             $cacheCrosierCoreAssetExtension->clear();
-            
-            if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-                return new RedirectResponse($targetPath);
-            }
+
             return new RedirectResponse($this->router->generate('index'));
         } catch (\Throwable $e) {
             $this->logger->error('Erro em onAuthenticationSuccess');
@@ -133,10 +121,64 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         }
     }
 
-    protected function getLoginUrl()
+
+    /**
+     * @required
+     * @param RouterInterface $router
+     */
+    public function setRouter(RouterInterface $router): void
     {
-        return $this->router->generate('login');
+        $this->router = $router;
+    }
+
+    /**
+     * @required
+     * @param CsrfTokenManagerInterface $csrfTokenManager
+     */
+    public function setCsrfTokenManager(CsrfTokenManagerInterface $csrfTokenManager): void
+    {
+        $this->csrfTokenManager = $csrfTokenManager;
+    }
+
+    /**
+     * @required
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function setPasswordEncoder(UserPasswordEncoderInterface $passwordEncoder): void
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @required
+     * @param UserEntityHandler $userEntityHandler
+     */
+    public function setUserEntityHandler(UserEntityHandler $userEntityHandler): void
+    {
+        $this->userEntityHandler = $userEntityHandler;
+    }
+
+    /**
+     * @required
+     * @param Security $security
+     */
+    public function setSecurity(Security $security): void
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * @required
+     * @param LoggerInterface $logger
+     */
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
     }
 
 
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new RedirectResponse('/login');
+    }
 }
