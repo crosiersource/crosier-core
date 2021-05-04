1a8923141c9b34a43da6269fdcc97e0c961e7d80
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-05-04 15:49:19 -0300

    Removendo CSSs do primevue do layout.js

--- assets/js/crosier/layout.js
+++ assets/js/crosier/layout.js
@@ -1,9 +1,12 @@
 import $ from "jquery";
 
 import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import "popper.js";
 import "perfect-scrollbar";
+
 import "@coreui/coreui";
+import "@coreui/coreui/dist/css/coreui.css";
 
 import "select2";
 import "select2/dist/js/i18n/pt-BR.js";
@@ -12,9 +15,6 @@ import CrosierBaseLayout from "./CrosierBaseLayout";
 import CrosierMasks from "./CrosierMasks";
 import DatatablesJs from "./DatatablesJs";
 
-import "primevue/resources/themes/saga-blue/theme.css"; // theme
-import "primevue/resources/primevue.min.css"; // core css
-import "primeicons/primeicons.css";
 import "select2/dist/css/select2.css";
 import "select2-bootstrap-theme/dist/select2-bootstrap.css";
 import "datatables.net-bs4/css/dataTables.bootstrap4.css";
@@ -24,8 +24,6 @@ import "toastr/build/toastr.css";
 import "daterangepicker/daterangepicker.css";
 
 import "../../static/css/crosier.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "@coreui/coreui/dist/css/coreui.css";
 
 $(document).ready(function init() {
   CrosierMasks.maskAll();
