635bdbe71059a14f31737f3fb73209aa38cfb4f7
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2022-03-04 00:08:43 -0300

    ...

--- sql/CREATE_DATABASE-mysql8.sql
+++ sql/CREATE_DATABASE-mysql8.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE crosier_precobaixo_dev CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
-CREATE USER 'crosier_precobaixo_dev'@'localhost' IDENTIFIED BY 'crosier_precobaixo_dev';
+CREATE DATABASE crosier_dev CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+CREATE USER 'crosier_dev'@'localhost' IDENTIFIED BY 'crosier_dev';
 -- ALTER USER 'crosier_dev'@'localhost' IDENTIFIED BY 'crosier_dev';
-GRANT ALL PRIVILEGES ON crosier_precobaixo_dev.* TO 'crosier_precobaixo_dev'@'localhost';
+GRANT ALL PRIVILEGES ON crosier_dev.* TO 'crosier_dev'@'localhost';
 FLUSH PRIVILEGES;
