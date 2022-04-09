03b186f22f4c9ebf96711fa65c42ba6da4c1496d
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2022-04-09 19:51:15 -0300

    ...

--- config/bundles.php
+++ config/bundles.php
@@ -7,7 +7,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
     CrosierSource\CrosierLibBaseBundle\CrosierLibBaseBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
