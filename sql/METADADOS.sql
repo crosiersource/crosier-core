36e7c59770c2de1d678c85b3f6d02ad923be6690
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2022-04-27 16:34:19 -0300

    ...

--- sql/METADADOS.sql
+++ sql/METADADOS.sql
@@ -437,20 +437,6 @@ CREATE TABLE `cfg_pushmessage`
 
 
 
-DROP TABLE IF EXISTS `cfg_entity_change`;
-
-CREATE TABLE `cfg_entity_change`
-(
-  `id`               bigint(20) AUTO_INCREMENT NOT NULL,
-  `entity_class`     varchar(200)              NOT NULL,
-  `entity_id`        bigint(20)                NOT NULL,
-  `changing_user_id` bigint(20)                NOT NULL,
-  `changed_at`       datetime                  NOT NULL,
-  `changes`          TEXT                      NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `K_cfg_entity_change_entity_class` (`entity_class`)
-) ENGINE = InnoDB;
-
 
 
 DROP TABLE IF EXISTS `bse_municipio`;
