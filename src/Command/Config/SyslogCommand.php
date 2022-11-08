f49175b10de1e7dca8648590376ec9d6739a9bea
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2022-11-08 08:31:21 -0300

    Corrigindo o crosier:syslog deletar_antigos

--- src/Command/Config/SyslogCommand.php
+++ src/Command/Config/SyslogCommand.php
@@ -2,23 +2,25 @@
 
 namespace App\Command\Config;
 
-use Doctrine\DBAL\DBALException;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * @author Carlos Eduardo Pauluk
- * @package App\Command\Config
+ * author Carlos Eduardo Pauluk
+ * 
+ * php bin/console crosier:syslog deletar_antigos
+ * 
  */
 class SyslogCommand extends Command
 {
 
-    private EntityManagerInterface $doctrine;
+    private ManagerRegistry $doctrine;
 
-    public function __construct(EntityManagerInterface $doctrine)
+    public function __construct(ManagerRegistry $doctrine)
     {
         $this->doctrine = $doctrine;
         parent::__construct();
@@ -54,7 +56,7 @@ class SyslogCommand extends Command
      */
     public function deletarAntigos(OutputInterface $output)
     {
-        $conn = $this->doctrine->getConnection();
+        $conn = $this->doctrine->getConnection('default');
         $rDias = $conn->fetchAllAssociative('SELECT valor FROM cfg_app_config WHERE app_uuid = :appUUID AND chave = :chave',
             [
                 'appUUID' => $_SERVER['CROSIERAPP_UUID'],
@@ -66,7 +68,8 @@ class SyslogCommand extends Command
         }
 
         try {
-            $conn->executeStatement('DELETE FROM cfg_syslog WHERE moment < DATE_SUB(NOW(),INTERVAL ' . (int)$rDias[0]['valor'] . ' DAY)');
+            $connLogs = $this->doctrine->getConnection('logs');
+            $connLogs->executeStatement('DELETE FROM cfg_syslog WHERE moment < DATE_SUB(NOW(),INTERVAL ' . (int)$rDias[0]['valor'] . ' DAY)');
         } catch (\Throwable $e) {
             throw new \RuntimeException('Erro ao deletar syslogs');
         }
