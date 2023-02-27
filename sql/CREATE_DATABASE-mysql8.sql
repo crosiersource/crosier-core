f315ce2b29acf8bbe38982223e42092bb9ea3f52
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-02-27 17:20:35 -0300

    ...

--- sql/CREATE_DATABASE-mysql8.sql
+++ sql/CREATE_DATABASE-mysql8.sql
@@ -6,7 +6,18 @@ FLUSH PRIVILEGES;
 
 
 CREATE DATABASE crosier_logs_dev CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
-CREATE USER 'crosier_logs_dev'@'localhost' IDENTIFIED BY 'crosier_logs_dev';
+CREATE USER 'crosier_logs_dev'@'%' IDENTIFIED BY 'crosier_logs_dev';
 -- ALTER USER 'crosier_dev'@'localhost' IDENTIFIED BY 'crosier_dev';
-GRANT ALL PRIVILEGES ON crosier_logs_dev.* TO 'crosier_logs_dev'@'localhost';
+GRANT ALL PRIVILEGES ON crosier_logs_dev.* TO 'crosier_logs_dev'@'%';
 FLUSH PRIVILEGES;
+
+
+-- dbname=crosier_pltr_dev_test
+-- mysql_config_editor set --login-path=$dbname --host=localhost --user=$dbname --password
+
+-- sudo touch /usr/local/bin/$dbname
+-- sudo echo "mysql --login-path=$dbname $dbname" | sudo tee /usr/local/bin/$dbname
+-- sudo chmod a+x /usr/local/bin/$dbname
+
+
+
