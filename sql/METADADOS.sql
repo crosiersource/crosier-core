82e3a0dd36057f6557d2ecb1767c84f71704f502
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-06-28 16:35:25 -0300

    Deixando <Dialog> do primevue sempre acima de todos com o z-index: 999999

--- sql/METADADOS.sql
+++ sql/METADADOS.sql
@@ -476,3 +476,14 @@ CREATE TABLE `bse_diautil`
 
 
 # ALTER TABLE cfg_pushmessage ADD `dt_validade` datetime AFTER dt_abert;
+
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+CREATE TABLE `doctrine_migration_versions`
+(
+  `version`        varchar(191) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at`    datetime DEFAULT NULL,
+  `execution_time` int      DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb3
+  COLLATE = utf8mb3_unicode_ci;
