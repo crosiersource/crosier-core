897aa40dbb42b9cef521a10bdc1be21bd99c4ae0
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-10-11 09:48:18 -0300

    Removendo endpoints diaUtil/obter....

--- src/Controller/Base/DiaUtilController.php
+++ src/Controller/Base/DiaUtilController.php
@@ -12,9 +12,7 @@ use CrosierSource\CrosierLibBaseBundle\Utils\DateTimeUtils\DateTimeUtils;
 use CrosierSource\CrosierLibBaseBundle\Utils\RepositoryUtils\FilterData;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -115,62 +113,6 @@ class DiaUtilController extends FormListController
         $corrigir = $request->get('corrigir') ?? false;
         $this->diaUtilBusiness->gerarOuCorrigirDiasUteis($dtIni, $dtFim, $corrigir);
         return CrosierApiResponse::success();
-    }
-
-    /**
-     * @Route("/api/bse/diaUtil/obter", name="api_bse_diaUtil_obter")
-     * @IsGranted("IS_AUTHENTICATED_REMEMBERED", statusCode=403)
-     */
-    public function obter(Request $request): JsonResponse
-    {
-        try {
-            $dt = DateTimeUtils::parseDateStr($request->get('dt'));
-            $proximo = filter_var($request->get('proximo'), FILTER_VALIDATE_BOOLEAN);
-            $qtdeMeses = $request->get('qtdeMeses') ?? 1;
-            /** @var DiaUtilRepository $repo */
-            $repo = $this->doctrine->getRepository(DiaUtil::class);
-
-            $diaUtil = $repo->findDiaUtil(DateTimeUtils::incMes($dt, $qtdeMeses), $proximo, true, true, true);
-            $dia = $diaUtil->format('Y-m-d');
-            return CrosierApiResponse::success($dia);
-        } catch (\Exception $e) {
-            return CrosierApiResponse::error(null, false, 'Ocorreu um erro ao buscar o dia útil');
-        }
-    }
-
-    
-    /**
-     * @Route("/api/bse/diaUtil/obter3", name="api_bse_diaUtil_obter3")
-     * @IsGranted("IS_AUTHENTICATED_REMEMBERED", statusCode=403)
-     */
-    public function obter3(Request $request): JsonResponse
-    {
-        try {
-            $dt = DateTimeUtils::parseDateStr($request->get('dt'));
-            $diasEmissao = (int)$request->get('diasEmissao');
-            $diasSolicitacao = (int)$request->get('diasSolicitacao');
-            
-            /** @var DiaUtilRepository $repo */
-            $repo = $this->doctrine->getRepository(DiaUtil::class);
-
-            $proximo = false;
-            
-            $dtMovimentacao = $repo->findDiaUtil(DateTimeUtils::incMes($dt, 1), $proximo, true, true, true);
-            
-            $dtSolicitacaoBruta = DateTimeUtils::addDays($dtMovimentacao, -$diasSolicitacao);
-            $dtSolicitacao = $repo->findDiaUtil($dtSolicitacaoBruta, false, true, true, true);
-
-            $dtEmissaoBruta = DateTimeUtils::addDays($dtMovimentacao, $diasEmissao);
-            $dtEmissao = $repo->findDiaUtil($dtEmissaoBruta, false, true, true, true);
-            
-            return CrosierApiResponse::success([
-                'dtMovimentacao' => $dtMovimentacao->format('Y-m-d'),
-                'dtSolicitacao' => $dtSolicitacao->format('Y-m-d'),
-                'dtEmissao' => $dtEmissao->format('Y-m-d'),
-            ]);
-        } catch (\Exception $e) {
-            return CrosierApiResponse::error(null, false, 'Ocorreu um erro ao buscar o dia útil');
-        }
     }
 
 
